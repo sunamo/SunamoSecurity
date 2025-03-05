83a021517594c9ed3cda33cdb47a65577de4d9f6
sunamo.cz <radek.jancik@sunamo.cz>
2025-03-05 07:29:20 +0100

    Added global using of Microsoft.Extensions.Logging.Abstractions to all projects

--- ProtectedDataHelper.cs
+++ ProtectedDataHelper.cs
@@ -1,30 +1,21 @@
 namespace SunamoSecurity;
-using SunamoSecurity;
-using SunamoSecurity._sunamo;
-using System.Security;
-using System.Security.Cryptography;
-using System.Text;
 
 public static class ProtectedDataHelper
 {
     public static string? EncryptString(string salt, SecureString input)
     {
         byte[] entropy = Encoding.Unicode.GetBytes(salt);
-
         var insecureString = SecureStringHelper.ToInsecureString(input);
-
         if (insecureString == null)
         {
             return null;
         }
-
         byte[] encryptedData = ProtectedData.Protect(
             Encoding.Unicode.GetBytes(insecureString),
             entropy,
             DataProtectionScope.CurrentUser);
         return Convert.ToBase64String(encryptedData);
     }
-
     public static SecureString DecryptString(string salt, string encryptedData)
     {
         try
