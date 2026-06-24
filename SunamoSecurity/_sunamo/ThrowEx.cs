72bdd9d0d2822376d525b85c5d721b3ad178d2e0
smutekutek <smutekutek@gmail.com>
2026-06-24 23:31:30 +0200

    Ahoj! Jak ti mohu pomoci?

--- SunamoSecurity/_sunamo/ThrowEx.cs
+++ SunamoSecurity/_sunamo/ThrowEx.cs
@@ -1,14 +1,7 @@
 namespace SunamoSecurity._sunamo;
 
-/// <summary>
-/// Internal helper for throwing exceptions with debugger breakpoints.
-/// </summary>
 internal class ThrowEx
 {
-    /// <summary>
-    /// Breaks the debugger and throws an exception with the specified message.
-    /// </summary>
-    /// <param name="message">The exception message to throw.</param>
     internal static void Custom(string message)
     {
         Debugger.Break();
